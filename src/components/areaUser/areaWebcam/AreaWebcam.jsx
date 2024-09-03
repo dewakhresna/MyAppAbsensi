c18e2c279df03b2d75c268c06b6e8b5537a94879
adiwira09 <adiwira009@gmail.com>
2024-09-03 15:35:09 +0700

    add popup dialog

--- src/components/areaUser/areaWebcam/AreaWebcam.jsx
+++ src/components/areaUser/areaWebcam/AreaWebcam.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
+import Swal from "sweetalert2";
 import "./areaWebcam.scss";
 
 const AreaWebcam = () => {
   const webcamRef = useRef(null);
+  const [loading, setLoading] = useState(false)
 
   // Fungsi untuk menangkap gambar dari webcam
   const capture = () => {
@@ -11,6 +13,7 @@ const AreaWebcam = () => {
     // console.log("Gambar yang Ditangkap:", imageSrc);
     if (imageSrc) {
       console.log("Sending image to API");
+      setLoading(true) // Set loading to true sebelum memulai fetch
       sendImageToAPI(imageSrc);
     }
   };
@@ -25,14 +28,50 @@ const AreaWebcam = () => {
         },
         body: JSON.stringify({
           image: imageSrc,
-          no_induk: "22222" // ganti dengan ID yang sesuai
+          no_induk: "11111" // ganti dengan ID yang sesuai
         }),
       });
 
       const data = await response.json();
       console.log("Respons API:", data);
+
+      // Menampilkan SweetAlert2 berdasarkan response API
+      if (data.status === "success"){
+        if (data.response.verified){
+          Swal.fire({
+            title: "Success!",
+            text: "Wajah Terverifikasi.",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+        }
+        else {
+          Swal.fire({
+            title: "Verification Failed!",
+            text: "Wajah Anda Tidak Sesuai. Mohon Ulangi Lagi.",
+            icon: "error",
+            confirmButtonText: "OK",
+          });
+        }
+      }
+      else if (data.status === "error"){
+        Swal.fire({
+          title: "Error!",
+          text: data.response || "Terjadi kesalahan saat memproses gambar.",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      }
     } catch (error) {
       console.error("Error mengirim gambar ke API:", error);
+      Swal.fire({
+        title: "Error!",
+        text: "Terjadi kesalahan saat mengirim gambar.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    } finally {
+      setLoading(false) // Set loading ke false setelah selesai
     }
   };
 
@@ -52,6 +91,13 @@ const AreaWebcam = () => {
         ref={webcamRef}
       />
       <button onClick={capture}>Ambil Gambar</button>
+
+      {/* Popup Loading */}
+      {loading && (
+        <div className="loading-popup">
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 };
