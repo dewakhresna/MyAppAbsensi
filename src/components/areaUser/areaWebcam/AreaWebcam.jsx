f48ecb6d5660d36f374a20352764650fd1b43c39
adiwira09 <adiwira009@gmail.com>
2024-09-04 14:51:47 +0700

    face detection with button

--- src/components/areaUser/areaWebcam/AreaWebcam.jsx
+++ src/components/areaUser/areaWebcam/AreaWebcam.jsx
@@ -1,11 +1,69 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
 import Swal from "sweetalert2";
 import "./areaWebcam.scss";
 
 const AreaWebcam = () => {
   const webcamRef = useRef(null);
-  const [loading, setLoading] = useState(false)
+  const canvasRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [isFaceDetected, setIsFaceDetected] = useState(false); // State untuk deteksi wajah
+
+  useEffect(() => {
+    // Load face-api.js models
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+      await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+      await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+      setModelsLoaded(true);
+    };
+    loadModels();
+  }, []);
+
+  const handleVideoPlay = () => {
+    const video = webcamRef.current.video;
+    const canvas = canvasRef.current;
+
+    // Set canvas size to match video size
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const options = new faceapi.TinyFaceDetectorOptions({
+      scoreThreshold: 0.6
+    });
+
+    // Start detecting faces
+    const detectFace = async () => {
+      if (modelsLoaded) {
+        // console.log("Model Loaded")
+        const detections = await faceapi.detectAllFaces(video, options)
+          .withFaceLandmarks();
+
+        // Clear canvas and draw the detections
+        const displaySize = { width: video.videoWidth, height: video.videoHeight };
+        faceapi.matchDimensions(canvas, displaySize);
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+        if (resizedDetections.length == 1) {
+          setIsFaceDetected(true);
+        } else{
+          setIsFaceDetected(false);
+        }
+      }
+      requestAnimationFrame(detectFace);
+    };
+
+    detectFace();
+  };
 
   // Fungsi untuk menangkap gambar dari webcam
   const capture = () => {
@@ -89,8 +147,16 @@ const AreaWebcam = () => {
         screenshotFormat="image/jpeg"
         videoConstraints={videoOptions}
         ref={webcamRef}
+        onPlay={handleVideoPlay}
       />
-      <button onClick={capture}>Ambil Gambar</button>
+      <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+      <button
+        onClick={capture}
+        className={isFaceDetected ? 'enabled' : 'disabled'}
+        disabled={!isFaceDetected}
+      >
+        Ambil Gambar
+      </button>
 
       {/* Popup Loading */}
       {loading && (
